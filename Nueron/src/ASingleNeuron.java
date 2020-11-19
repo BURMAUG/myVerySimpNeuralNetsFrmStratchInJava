36b54f810ed43832f1c5c3692e6f340ca421009f
Deejai <burmaud1@gmail.com>
2020-11-18 21:59:59 -0500

    single neuron
    this is both the inefficient and efficient way of going about it.

--- Nueron/src/ASingleNeuron.java
+++ Nueron/src/ASingleNeuron.java
@@ -15,6 +15,10 @@ public class ASingleNeuron {
         inputs.add(0.8);
         inputs.add(-0.5);
         double result = 0;
+        //trial in a line and inefficient way of going about it.
+        double out = weights.get(0) * inputs.get(0) + weights.get(1)*inputs.get(1) + weights.get(2)*inputs.get(2) + 2;
+        System.out.println("put: " + out);
+
         for(int i = 0; i < weights.size(); i++){
            result += (weights.get(i) * inputs.get(i)) ;
         }
